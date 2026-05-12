489d7c43c192fced318aacdabfd51585f2119957
Robert Leist <robert.leist@dfki.de>
2026-05-12 11:24:23 +0200

    Implement inference endpoint for instance segmentation with model validation

--- app/routes/inference.py
+++ app/routes/inference.py
@@ -1,15 +1,21 @@
-from fastapi import APIRouter
-from app.state import MODEL_REGISTRY
-from iquana_toolbox.schemas.networking.http.services import BaseServiceRequest
+from fastapi import APIRouter, HTTPException
+from iquana_toolbox.schemas.database.contours import Contour
 
+from app.state import MODEL_REGISTRY
+from iquana_toolbox.schemas.networking.http.services import InstanceSegmentationRequest
+
+from models.base_model import BaseInstanceSegmentationModel
+from util.validate_model import validate_model
 
 router = APIRouter()
 
 @router.post("/inference")
-async def inference(request: BaseServiceRequest):
-    """ Load a model from mlflow registry and perform inference on the provided image. This is a placeholder implementation. The actual logic will depend on the model and inference requirements."""
-    # Load a model like this. Note that this caches the loading, too!
-    model = MODEL_REGISTRY.get_model(request.model_registry_key)
-    # TODO: Implement your inference!
-    raise NotImplementedError("Inference endpoint not implemented yet")
-
+async def inference(request: InstanceSegmentationRequest) -> list[Contour]:
+    """
+        Load a model from mlflow registry and perform inference on the provided image.
+        This is a placeholder implementation. The actual logic will depend on the model and inference requirements.
+    """
+    # Validates the model selection
+    validate_model(request)
+    model: BaseInstanceSegmentationModel = MODEL_REGISTRY.get_model_by_alias(request.model_registry_key, "latest")
+    return model.inference(request)
